55bc337b0982817fb737fc1dd4ff0030da2cf027
vikash kumar <vikashekma151299@gmail.com>
2023-07-25 17:44:42 +0530

    vikash_singh_7070935505_assignment_full_satack

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,92 @@
-function App() {
-	return (
-		<h1>Hello world</h1>
-	);
-}
+import React, { useState } from 'react';
+import axios from 'axios';
+import './styles.css'; // Import the CSS file
+const App = () => {
+  const [symbolRow, setSymbolRow] = useState('');
+  const [date, setDate] = useState('');
+  const [stockData, setStockData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      setError(null);
+        const symbol = symbolRow.toUpperCase();
+      // Make a request to the server API
+      const response = await axios.post('http://localhost:5000/api/fetchStockData', { symbol, date });
+
+      // Update the state with the received stock data
+      setStockData(response.data);
+    } catch (error) {
+      console.error('Error fetching stock data:', error);
+      setError('An error occurred while fetching stock data. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="heading">Stock Trade Statistics</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <label className="form-label">
+          Stock Symbol:
+          <input
+            type="text"
+            value={symbolRow}
+            onChange={(e) => setSymbolRow(e.target.value)}
+            required
+            className="form-input"
+            placeholder="Enter stock symbol"
+          />
+        </label>
+        <label className="form-label">
+          Date:
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+            className="form-input"
+          />
+        </label>
+        <button type="submit" className="form-button" disabled={loading}>
+          {loading ? 'Fetching...' : 'Fetch Stock Data'}
+        </button>
+      </form>
+
+      {error && <p className="error">{error}</p>}
+
+      {stockData && (
+        <div className="stock-data">
+          <h2 className="stock-data-heading">Stock Data</h2>
+          <table className="stock-table">
+            <thead>
+              <tr>
+                <th>Open</th>
+                <th>High</th>
+                <th>Low</th>
+                <th>Close</th>
+                <th>Volume</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{stockData.open}</td>
+                <td>{stockData.high}</td>
+                <td>{stockData.low}</td>
+                <td>{stockData.close}</td>
+                <td>{stockData.volume}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require("body-parser");
 const axios = require("axios");
-
 const app = express();
 
 app.use(function (req, res, next) {
@@ -26,9 +25,33 @@ app.use(bodyParser.json());
 
 app.enable('trust proxy');
 
-app.post('/api/fetchStockData', (req, res) => {
+app.post('/api/fetchStockData', async (req, res) => {
     // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
-    res.sendStatus(200);
+    try {
+        const { symbol, date } = req.body;
+    
+        if (!symbol || !date) {
+          return res.status(400).json({ error: 'Symbol and date are required' });
+        }
+    
+        const polygonApiKey = process.env.POLYGON_API_KEY; // API key should be in .env file the environment variable
+        if (!polygonApiKey) {
+          return res.status(500).json({ error: 'Polygon API key is missing' });
+        }
+       
+        // Make a request to the Polygon API with the symbol and date parameters
+        const response = await axios.get(`https://api.polygon.io/v1/open-close/${symbol}/${date}?adjusted=true&apiKey=${polygonApiKey}`);
+      
+        // Extract the required fields from the response
+        const { open, high, low, close, volume } = response.data;
+    
+        // Send the required fields in the response
+        res.json({ open, high, low, close, volume });
+      } catch (error) {
+        console.error('Error fetching stock data:', error);
+        res.status(500).json({ error: 'Unable to fetch stock data' });
+      }
+    // res.sendStatus(200);
 });
 
 const port = process.env.PORT || 5000;
